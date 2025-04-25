3c6f4e40fa42673f40468c55cbd05317905fbd6c
Srinidhi Yoganand <120911846+Srinidhi-Yoganand@users.noreply.github.com>
2025-04-25 16:46:59 +0530

    feat: convert mp to wav and extract PCM samples

--- src/main/java/org/sadp/audiorecognition/service/AudioService.java
+++ src/main/java/org/sadp/audiorecognition/service/AudioService.java
@@ -2,19 +2,39 @@ package org.sadp.audiorecognition.service;
 
 import lombok.RequiredArgsConstructor;
 import org.sadp.audiorecognition.util.AudioUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+
 @Service
 @RequiredArgsConstructor
 public class AudioService {
+    private static final Logger log = LoggerFactory.getLogger(AudioService.class);
     private final AudioUtils audioUtils;
     private final SpectrogramService spectrogramService;
     private final FingerprintService fingerprintService;
 
     public String processUploadedAudio(MultipartFile file){
-        return "Stub: uploaded"+ file.getOriginalFilename();
+        try{
+            File wav=audioUtils.convertMp3ToWav(file);
+//            System.out.println("Audio Service: Converted MP3 to WAV");
+            float[] pcm=audioUtils.extractPcmSamples(wav);
+
+//            log.info("Extracted {} PCM Sample", pcm.length);
+//            for(int i=0;i<10;i++){
+//                log.info("Sample {}: {}", i, pcm[i]);
+//            }
+
+            wav.delete();
+            return "Extracted "+pcm.length+" PCM Samples";
+        }catch(Exception e){
+//            log.error("Error processing audio file", e);
+            return "Error processing audio file: " + e.getMessage();
+        }
     }
 
     public String matchAudio(MultipartFile file){
